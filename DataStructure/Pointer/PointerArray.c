dc63e40e1e548e1f8b22f9886d6cdc52d238dc04
yunseong76039287 <dbsdltjd3701@naver.com>
2021-10-11 15:23:44 +0900

    포인터 복습

---
+++ DataStructure/Pointer/PointerArray.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num1 =10, num2 = 20, num3 = 30;
+    int* arr[3] = {&num1, &num2, &num3};
+    
+    system("pause");
+    return 0;
+}
+
+
