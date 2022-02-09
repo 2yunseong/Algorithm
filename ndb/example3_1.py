662db52d0e3ec72d07d37bd7b674554cd2bf1e2e
2yunseong <dbsdltjd3701@naver.com>
2022-02-10 06:17:30 +0900

    feat: 예제 3.1. 거스름돈

---
+++ ndb/example3_1.py
@@ -0,0 +1,22 @@
+
+money = int(input())
+coinCount = 0
+
+while money != 0:
+    if money >= 500:
+        coinCount = coinCount + 1
+        money = money - 500
+        continue
+    if money >= 100:
+        coinCount = coinCount + 1
+        money = money - 100
+        continue
+    if money >= 50:
+        coinCount = coinCount + 1
+        money = money - 50
+        continue
+    if money >= 10:
+        coinCount = coinCount + 1
+        money = money - 10
+
+print(coinCount)
