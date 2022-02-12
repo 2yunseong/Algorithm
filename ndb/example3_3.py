f25b3c98692372e4f6511bcfb309e1cd7a1e6604
2yunseong <dbsdltjd3701@naver.com>
2022-02-12 21:13:39 +0900

    feat : 3.3. 숫자 카드 게임 풀이

---
+++ ndb/example3_3.py
@@ -0,0 +1,14 @@
+
+n, m = map(int, input().split())
+global_max = -1
+
+for i in range(n):
+    row_arr = list(map(int, input().split()))
+    local_min = 999999
+    for j in range(m):
+        if(local_min > row_arr[j]):
+            local_min = row_arr[j]
+    if global_max < local_min:
+        global_max = local_min
+
+print(global_max)
