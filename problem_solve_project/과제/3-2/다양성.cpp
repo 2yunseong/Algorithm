dc7d18be158a1ab19766c027030d68b00d0dc802
2yunseong <dbsdltjd3701@naver.com>
2022-03-18 00:58:23 +0900

    feat : 주석추가

--- problem_solve_project/과제/3-2/다양성.cpp
+++ problem_solve_project/과제/3-2/다양성.cpp
@@ -8,8 +8,9 @@ int main()
     int current; // 최근 화보의 고유넘버
     int count = 0;
     cin >> n;
-    int *arr = new int[n];
+    int *arr = new int[n]; // 화보를 받아올 배열 생성
 
+    // 화보 고유번호 입력 받아오기
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
@@ -17,11 +18,13 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
+        // 화보의 갯수가 1개가 보장되므로, i 가 0일때는 일단 세어준다.
         if (i == 0)
         {
             count++;
             current = arr[i];
         }
+        // 화보가 오름차순으로 정렬되어있으므로. 현재의 값과 다르면 다른 화보이므로 카운트한다.
         if (current != arr[i])
         {
             count++;
@@ -29,7 +32,7 @@ int main()
         }
     }
 
-    cout << count;
+    cout << count; // 답 출력
     delete[] arr;
     return 0;
 }
